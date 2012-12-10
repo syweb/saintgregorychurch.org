f82a130a83f3d0c9c5cf44d1189ae4f4ae8eab30
Chintan Sheth <chintansheth@Chintan.local>
2012-12-10 15:29:35 +0530

    [Update] Setup Vlad

---
+++ config/deploy.rb
@@ -0,0 +1,14 @@
+set :repository,  "git@github.com:syweb/saintgregorychurch.org.git"
+set :user,        "sites"
+set :domain,      "#{user}@insightmethods.com"
+set :deploy_to,   "/home/sites/saintgregorychurch.org"
+set :port,          4000
+set :merb_env,      "preview"
+set :processes,     1
+
+
+
+namespace :vlad do
+
+end
+
